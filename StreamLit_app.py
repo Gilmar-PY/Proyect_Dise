b603d9d9952fa8f84dc8df8bc8b516a500603608
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2023-07-03 10:33:10 -0500

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -1,24 +1,23 @@
 import streamlit as st
 # Configuración de la interfaz de Streamlit
 st. title ("Lectura de datos desde Arduino")
-imagen
-st. empty() # Contenedor para mostrar la imagen
+imagen=st.empty() ### contenedor para mostrar la imagen
+ # Contenedor para mostrar la imagen
 # Lee los datos del serial
 while True:
-if arduino. in _waiting > 0:
-# Lee una línea de datos del serial y la decodifica
-dato
-arduino.readline().decode().rstrip()
-# Actualiza la gráfica
-x. append(Len(x) 1)
-y. append (float (dato))
-ax. clear()
-ax.plot(x, y)
-ax.set xlabel('Tiempo',
-ax.set_ylabel('Valor de la humedad en %')
-• Actualiza la imagen en Streamlit
-imagen.pyplot(fig)
-plt. pause(0. 1)
+  if arduino. in _waiting > 0
+    # Lee una línea de datos del serial y la decodifica
+    arduino.readline().decode().rstrip()
+      # Actualiza la gráfica
+  x. append(Len(x) 1)
+  y. append (float (dato))
+  ax. clear()
+  ax.plot(x, y)
+  ax.set xlabel('Tiempo',
+  ax.set_ylabel('Valor de la humedad en %')
+  #Actualiza la imagen en Streamlit
+  imagen.pyplot(fig)
+  plt. pause(0. 1)
 st.write("hola mundo ")
 
 
